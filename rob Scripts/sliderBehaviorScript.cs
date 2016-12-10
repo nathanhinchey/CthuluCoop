cd44146d5a8398eb1fa19901ef925bb40f891b12
Robert Rappoport <rorappo@gmail.com>
2016-12-10 18:05:49 -0500

    Updated slider scripts and scene

--- rob Scripts/sliderBehaviorScript.cs
+++ rob Scripts/sliderBehaviorScript.cs
@@ -4,14 +4,61 @@ using System.Collections;
 
 public class sliderBehaviorScript : MonoBehaviour {
 
+	private bool moneyIsDecreasing = true;
+	private bool mindIsDecreasing = true;
+	private bool bodyIsDecreasing = true;
 
-	private float money;
+	public float moneyEnergy = 100f;
 
 	public Slider Finance;
 
+	public float mindEnergy = 100f;
+
+	public Slider mind;
+
+	public float bodyEnergy = 100f;
+
+	public Slider body;
+
+	void Update ()
+	{
+		if (moneyIsDecreasing == true) 
+		{
+			moneyEnergy -= Time.deltaTime;
+		}
+
+		if (mindIsDecreasing == true) 
+		{
+			mindEnergy -= Time.deltaTime;
+		}
+
+		if (bodyIsDecreasing == true) 
+		{
+			bodyEnergy -= Time.deltaTime;
+		}
+
+		setFinance ();
+		setMind ();
+		setBody ();
+	}
+
 	public void setFinance()
 	{
-		Finance.value = money;
-		Debug.Log("money = " + money);
+		Finance.value = moneyEnergy;
+		Debug.Log("money = " + moneyEnergy);
 	}
+
+	public void setMind()
+	{
+		mind.value = mindEnergy;
+		Debug.Log("mind = " + mindEnergy);
+	}
+
+	public void setBody()
+	{
+		body.value = bodyEnergy;
+		Debug.Log("body = " + bodyEnergy);
+	}
+
+
 }
