be4735454a66af3ceaccacf0946ae6bcc543fb59
Nathan Hinchey <nathanhinchey@gmail.com>
2016-12-10 15:14:05 -0500

    Begin writing scripts.

---
+++ Scripts/GameData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameData : MonoBehaviour {
+	public static GameObject player;
+
+	// All quests in the game (not sure what this will be used for)
+	public static List<Quest> quests;
+
+	// Values for status bars
+	public static float mentalScore;
+	public static float physicalScore;
+	public static float financialScore;
+}
---
+++ Scripts/Quest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest : MonoBehaviour {
+	public string title;
+	public string text;
+
+	public Quest(string title, string text){
+		this.title = title;
+		this.text = text;
+	}
+}
