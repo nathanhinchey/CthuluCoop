6a98b7f859980a4062744b2f36c754c61b945955
Robert Rappoport <rorappo@gmail.com>
2016-12-10 15:11:15 -0500

    started slider scripts

---
+++ rob Scripts/sliderBehaviorScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class sliderBehaviorScript : MonoBehaviour {
+
+
+	private float money;
+
+	public Slider Finance;
+
+	public void setFinance()
+	{
+		Finance.value = money;
+		Debug.Log("money = " + money);
+	}
+}
