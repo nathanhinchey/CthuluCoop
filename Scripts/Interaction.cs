f1c2e257fb684464a77b7606116a4d285d90427e
Nathan Hinchey <nathanhinchey@gmail.com>
2016-12-10 17:40:30 -0500

    Improve interaction script and mainScene scene.

--- Scripts/Interaction.cs
+++ Scripts/Interaction.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Interaction : MonoBehaviour {
-
+	// Text that shows up on menu
 	public string decription;
 
+	// How much each bar will be changed
+	// Rewards are positive, costs are negative
+	public float financialEffect;
+	public float physicalEffect;
+	public float mentalEffect;
+
+
 	public void Activate(){
+		
 	}
 }
