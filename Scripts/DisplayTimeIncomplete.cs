e63f61dab0198a909a751fc80c7cd85b69040b88
Nathan Hinchey <nathanhinchey@gmail.com>
2016-12-10 21:10:14 -0500

    Make a not-quite-right digital clock.

---
+++ Scripts/DisplayTimeIncomplete.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class DisplayTimeIncomplete : MonoBehaviour {
+	private Text timeFromClock;
+	private int hour;
+	private int minute;
+
+	void Start () {
+		timeFromClock = gameObject.GetComponent<Text> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timeFromClock.text = Clock.GameHour.ToString () + ":" + Clock.GameMinute.ToString ();
+	}
+}
